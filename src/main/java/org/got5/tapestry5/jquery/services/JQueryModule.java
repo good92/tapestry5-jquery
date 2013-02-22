eb12bcc8a26efd2e116638d71540d436011c954a
good92 <Basile.CHANDESRIS@ext.ec.europa.eu>
2013-02-22 12:09:56 +0100

    Update module to support jquery version 1.9.1

--- src/main/java/org/got5/tapestry5/jquery/services/JQueryModule.java
+++ src/main/java/org/got5/tapestry5/jquery/services/JQueryModule.java
@@ -110,8 +110,8 @@ public class JQueryModule
         configuration.add(JQuerySymbolConstants.TAPESTRY_JS_PATH, "classpath:org/got5/tapestry5/tapestry.js");
 
 
-        configuration.add(JQuerySymbolConstants.JQUERY_CORE_PATH, "classpath:org/got5/tapestry5/jquery/jquery_core/jquery-1.7.2.js");
-        configuration.add(JQuerySymbolConstants.JQUERY_VERSION, "1.7.2");
+        configuration.add(JQuerySymbolConstants.JQUERY_CORE_PATH, "classpath:org/got5/tapestry5/jquery/jquery_core/jquery-1.9.1.js");
+        configuration.add(JQuerySymbolConstants.JQUERY_VERSION, "1.9.1");
 
 
         configuration.add(JQuerySymbolConstants.JQUERY_UI_PATH, "classpath:org/got5/tapestry5/jquery/ui_1_8_24");
